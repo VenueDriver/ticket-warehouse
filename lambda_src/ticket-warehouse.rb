1f2661bf7f8599dda99798c4e02e6b6b5ea8ed4f
rcsteins <rcsteins@gmail.com>
2023-10-20 12:36:02 -0400

    fix whitespace

--- lambda_src/ticket-warehouse.rb
+++ lambda_src/ticket-warehouse.rb
@@ -47,31 +47,31 @@ class TicketWarehouse
     end
   end
 
-def fetch_events(organization_id: nil, start_before: nil, start_after: nil)
-  params = {}
-  params[:organization_id] = organization_id if organization_id
-  params[:start_before] = start_before if start_before
-  params[:start_after] = start_after if start_after
-  
-  query_string = params.empty? ? '' : '?' + URI.encode_www_form(params)
-  
-  fetch_api_data("https://api.ticketsauce.com/v2/events#{query_string}")
-end
+  def fetch_events(organization_id: nil, start_before: nil, start_after: nil)
+    params = {}
+    params[:organization_id] = organization_id if organization_id
+    params[:start_before] = start_before if start_before
+    params[:start_after] = start_after if start_after
+    
+    query_string = params.empty? ? '' : '?' + URI.encode_www_form(params)
+    
+    fetch_api_data("https://api.ticketsauce.com/v2/events#{query_string}")
+  end
 
-def fetch_orders(event:)
-  event_id = event['Event']['id']
-  fetch_api_data("https://api.ticketsauce.com/v2/orders/#{event_id}")
-end
+  def fetch_orders(event:)
+    event_id = event['Event']['id']
+    fetch_api_data("https://api.ticketsauce.com/v2/orders/#{event_id}")
+  end
 
-def fetch_order_details(order:)
-  order_id = order['Order']['id']
-  fetch_api_data("https://api.ticketsauce.com/v2/order/#{order_id}")
-end
+  def fetch_order_details(order:)
+    order_id = order['Order']['id']
+    fetch_api_data("https://api.ticketsauce.com/v2/order/#{order_id}")
+  end
 
-def fetch_checkin_ids(event:)
-  event_id = event['Event']['id']
-  fetch_api_data("https://api.ticketsauce.com/v2/tickets/checkin_ids/#{event_id}")
-end
+  def fetch_checkin_ids(event:)
+    event_id = event['Event']['id']
+    fetch_api_data("https://api.ticketsauce.com/v2/tickets/checkin_ids/#{event_id}")
+  end
 
   def archive_events(time_range: nil, num_threads: 4)
     puts "Archiving events for time range: #{time_range}"
