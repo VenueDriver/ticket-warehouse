dd9a6d99d145a028d89d46c78cd4af64bcd3a3a7
Derek Norrbom <dnorrbom@hakkasan.com>
2023-10-26 14:47:58 -0700

    use sdk for ssm values
    use sdk for ssm values

--- bin/ticket-warehouse.ts
+++ bin/ticket-warehouse.ts
@@ -3,50 +3,65 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { TicketWarehousePipelineStack } from '../lib/pipeline/ticket-warehouse-pipeline';
-import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 import * as dotenv from 'dotenv';
+import { SSM } from 'aws-sdk';
+
 dotenv.config();
 
-// utility function to get account IDs
-function getAccountId(name: string, fallbackName: string, scope: Construct): string {
-  return process.env[name] ? process.env[name]! : ssm.StringParameter.fromStringParameterAttributes(scope, `TicketWarehouse-${fallbackName}`, {
-    parameterName: fallbackName.toLowerCase(),
-    version: 1
-  }).stringValue;
-}
+// utility function to get account IDs asynchronously
+async function getAccountId(name: string, fallbackName: string): Promise<string> {
+  if (process.env[name]) return process.env[name]!;
 
-class TicketWarehousePipelineStackWrapper extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    const deployFromAccount = getAccountId('DEPLOYMENT_AWS_ACCOUNT_ID', 'DeploymentAWSAccountId', this);
-    const productionAccount = getAccountId('PRODUCTION_AWS_ACCOUNT_ID', 'ProductionAWSAccountId', this);
-    const stagingAccount = getAccountId('STAGING_AWS_ACCOUNT_ID', 'StagingAWSAccountId', this);
-    const developmentAccount = getAccountId('DEVELOPMENT_AWS_ACCOUNT_ID', 'DevelopmentAWSAccountId', this);
-
-    new TicketWarehousePipelineStack(this, 'ticket-warehouse-pipeline-staging', {
-      env: { account: deployFromAccount, region: 'us-east-1' },
-      Stage: 'staging',
-      AccountToDeployTo: stagingAccount,
-      DeploymentRegion: 'us-east-1'
-    });
-
-    new TicketWarehousePipelineStack(this, 'ticket-warehouse-pipeline-production', {
-      env: { account: deployFromAccount, region: 'us-east-1' },
-      Stage: 'production',
-      AccountToDeployTo: productionAccount,
-      DeploymentRegion: 'us-east-1'
-    });
-
-    new TicketWarehousePipelineStack(this, 'ticket-warehouse-pipeline-development', {
-      env: { account: deployFromAccount, region: 'us-east-1' },
-      Stage: 'development',
-      AccountToDeployTo: developmentAccount,
-      DeploymentRegion: 'us-east-1'
-    });
+  const ssmClient = new SSM();
+  try {
+    const result = await ssmClient.getParameter({ Name: fallbackName.toLowerCase() }).promise();
+    return result.Parameter?.Value || '';
+  } catch (err) {
+    console.error(`Error fetching SSM parameter ${fallbackName}:`, err);
+    return '';
   }
 }
 
-const app = new cdk.App();
-new TicketWarehousePipelineStackWrapper(app, 'TicketWarehousePipelineStackWrapper');
+async function main() {
+  const app = new cdk.App();
+
+  const deployFromAccount = await getAccountId('DEPLOYMENT_AWS_ACCOUNT_ID', 'DeploymentAWSAccountId');
+  const productionAccount = await getAccountId('PRODUCTION_AWS_ACCOUNT_ID', 'ProductionAWSAccountId');
+  const stagingAccount = await getAccountId('STAGING_AWS_ACCOUNT_ID', 'StagingAWSAccountId');
+  const developmentAccount = await getAccountId('DEVELOPMENT_AWS_ACCOUNT_ID', 'DevelopmentAWSAccountId');
+
+  class TicketWarehousePipelineStackWrapper extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+
+      new TicketWarehousePipelineStack(this, 'ticket-warehouse-pipeline-staging', {
+        env: { account: deployFromAccount, region: 'us-east-1' },
+        Stage: 'staging',
+        AccountToDeployTo: stagingAccount,
+        DeploymentRegion: 'us-east-1'
+      });
+
+      new TicketWarehousePipelineStack(this, 'ticket-warehouse-pipeline-production', {
+        env: { account: deployFromAccount, region: 'us-east-1' },
+        Stage: 'production',
+        AccountToDeployTo: productionAccount,
+        DeploymentRegion: 'us-east-1'
+      });
+
+      new TicketWarehousePipelineStack(this, 'ticket-warehouse-pipeline-development', {
+        env: { account: deployFromAccount, region: 'us-east-1' },
+        Stage: 'development',
+        AccountToDeployTo: developmentAccount,
+        DeploymentRegion: 'us-east-1'
+      });
+    }
+  }
+
+  new TicketWarehousePipelineStackWrapper(app, 'TicketWarehousePipelineStackWrapper');
+}
+
+main().catch(err => {
+  console.error('An error occurred:', err);
+  process.exit(1);
+});
