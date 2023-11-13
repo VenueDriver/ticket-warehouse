25893a91b916b5706fcc23dc797c856946f23f55
Ryan Alyn Porter <rap@endymion.com>
2023-11-13 20:07:40 +0000

    Draft of a ticket-warehouse-tickets dataset.

--- lib/manager/Quicksight.rb
+++ lib/manager/Quicksight.rb
@@ -94,7 +94,7 @@ module Manager
       
       client = Aws::QuickSight::Client.new(region: 'us-east-1')
 
-      name = 'ticket-warehouse-orders'
+      name = 'ticket-warehouse-tickets'
       puts "  Creating dataset for query for #{name}..."
 
       resp = client.create_data_set({
@@ -107,19 +107,47 @@ module Manager
               data_source_arn: data_source_arn,
               name: name,
               sql_query: <<~SQL,
-                select  tw_orders.venue
-                , tw_orders.year
-                , tw_orders.month
-                , tw_orders.day
-                , tw_orders."order".id as order_id
-                , tw_orders."order".status as order_status
-
-                , ticket_list.ticket_type_name
-                , ticket_list.ticket_type_id
-                from ticket_warehouse.ticket_warehouse_orders tw_orders,
-                UNNEST(tw_orders."Ticket") as t(ticket_list)
-                order by tw_orders."order".id
-                limit 6;
+                SELECT
+                  "Order".id AS order_id,
+                  Event.organization_name AS venue_title,
+                  Event.name AS event_title,
+                  CAST(CAST(Event.start AS TIMESTAMP) AS DATE) AS event_date,
+                  1 AS quantity,
+                  ticket_data.price AS shared_price_per_ticket,
+                  ticket_data.price AS face_value_subtotal,
+                  "Order".lineitemfees.surcharge AS shared_surcharge,
+                  "Order".lineitemfees.let_tax AS shared_let_tax_amount_currency,
+                  "Order".lineitemfees.sales_tax AS sales_tax_currency,
+                  "Order".lineitemfees.venue_fee AS service_charge_currency,
+                  0 AS donation_charity,
+                  (
+                    COALESCE(CAST(ticket_data.price AS DECIMAL(10, 2)), 0.00) + 
+                    COALESCE(CAST("Order".lineitemfees.surcharge AS DECIMAL(10, 2)), 0.00) + 
+                    COALESCE(CAST("Order".lineitemfees.let_tax AS DECIMAL(10, 2)), 0.00) + 
+                    COALESCE(CAST("Order".lineitemfees.sales_tax AS DECIMAL(10, 2)), 0.00) + 
+                    COALESCE(CAST("Order".lineitemfees.venue_fee AS DECIMAL(10, 2)), 0.00)
+                  ) AS total,
+                  ticket_data.ticket_type_name AS ticket_type_name,
+                  ticket_data.ticket_type_id AS ticket_type_id,
+                  ticket_data.name AS ticket_holder_name,
+                  "Order".first_name AS order_first_name,
+                  "Order".last_name AS order_last_name,
+                  "Order".paid_date_utc AS final_at_UTC,
+                  'false' AS void,
+                  'Stripe' AS payment_gateway,
+                  '' AS learned_from,
+                  ticket_data.id AS id,
+                  "Order".paid_date_utc AS created_at_UTC,
+                  "Order".event_id AS event_id,
+                  ticket_data.redeemed AS ticket_redeemed,
+                  ticket_data.promo_code AS ticket_promo_code
+              FROM 
+                  ticket_warehouse.ticket_warehouse_events AS Event
+              INNER JOIN
+                  ticket_warehouse.ticket_warehouse_orders AS "Order"
+                  ON Event.id = "Order".event_id
+              CROSS JOIN
+                  UNNEST("Order".ticket) AS t(ticket_data)
               SQL
               columns: [
                 { :name => "venue", :type => "STRING" },
