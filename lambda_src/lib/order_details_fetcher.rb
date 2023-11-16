a94eb26ab0d2c00e2c8d483983eb92ba8f800ce1
rcsteins <rcsteins@gmail.com>
2023-11-16 16:11:54 -0500

    Not using this class at the moment, but if we did it would be better to run these on the :fast executor

--- lambda_src/lib/order_details_fetcher.rb
+++ lambda_src/lib/order_details_fetcher.rb
@@ -11,7 +11,10 @@ class OrderDetailsFetcher
     task = Proc.new{|order_id| fetch_by_order_id(order_id)}
     if @use_promises
       orders_id_list.map do |order_id|
-        Concurrent::Promises.future(order_id, &task)
+        #Concurrent::Promises.future(order_id, &task) uses the :io executor
+        # which doesn't place any limits on the thread count
+        # here we want a fixed number of threads to rate limit the requests
+        Concurrent::Promises.future_on(:fast, order_id, &task)
       end.map{|future| future.value!  }
     else
       orders_id_list.map(&task)
