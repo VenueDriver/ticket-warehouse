af5b12e0d28695fccca5dbdfea7a70704bb0ae1e
Ryan Alyn Porter <rap@endymion.com>
2023-11-20 20:23:20 +0000

    athena:GetQueryResults
    You are not authorized to perform: athena:GetQueryResults on the resource. After your AWS administrator or you have updated your permissions, please try again.

--- lib/ticket-warehouse-stack.ts
+++ lib/ticket-warehouse-stack.ts
@@ -60,6 +60,7 @@ export class TicketWarehouseStack extends cdk.Stack {
         'athena:ListNamedQueries',
         'athena:StartQueryExecution',
         'athena:GetQueryExecution',
+        'athena:GetQueryResults',
         'glue:GetDatabase',
         'glue:CreateDatabase',
         'glue:CreateTable',
