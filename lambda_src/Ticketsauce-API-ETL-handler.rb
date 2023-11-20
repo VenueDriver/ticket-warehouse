50403c4a6c25c3986943e7022432defb30a6365d
Ryan Alyn Porter <rap@endymion.com>
2023-11-20 22:00:22 +0000

    I figured out why the Lambda function isn't archiving Stripe data.
    I never added the call for doing that to the Lambda function.  It was only in the manager.rb CLI.

--- lambda_src/Ticketsauce-API-ETL-handler.rb
+++ lambda_src/Ticketsauce-API-ETL-handler.rb
@@ -5,6 +5,8 @@ require_relative 'ticket-warehouse'
 def lambda_handler(event:, context:)
   puts "Received event: #{JSON.pretty_generate(event)}"
 
+  StripeArchiver.new.archive_charges(time_range:time_range)
+
   warehouse = TicketWarehouse.new(
     client_id:     ENV['TICKETSAUCE_CLIENT_ID'],
     client_secret: ENV['TICKETSAUCE_CLIENT_SECRET']
