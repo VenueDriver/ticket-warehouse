64b2aeba5467ec29eeafc91e55073e6baf0af028
rcsteins <rcsteins@gmail.com>
2023-12-07 17:24:08 -0500

    Update manifest.rb

--- lambda_src/lib/manifest/manifest.rb
+++ lambda_src/lib/manifest/manifest.rb
@@ -2,10 +2,15 @@ require_relative '../../athena-manager.rb'
 require_relative 'dev_query.rb'
 require_relative 'omnia_data_rows_example.rb'
 require_relative 'ticket_rows.rb'
+require_relative 'report_demo.rb'
 require 'erb'
 
 module Manifest
   class << self
+
+    def report_demo
+      ReportDemo.render_all_omnia_events
+    end
 
     def test_rendering
       template_file_path = File.join( File.dirname(__FILE__), 'template.html.erb' )
