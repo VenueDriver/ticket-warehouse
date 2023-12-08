71f9631da37c0b711a460bccadd9f058ec397838
rcsteins <rcsteins@gmail.com>
2023-12-08 16:42:39 -0500

    Update output_structs.rb
    add sales tax, venue_fee

--- lambda_src/lib/manifest/output_structs.rb
+++ lambda_src/lib/manifest/output_structs.rb
@@ -3,6 +3,7 @@ module Manifest
     TopLevelStruct = Struct.new(:event_date, :venue, :event_title, 
     :total_sold, :total_face_value, :total_let, :ticket_rows, 
     :now_in_pacific_time, :total_bar_card,
+    :total_sales_tax, :total_venue_fee, 
     :event_id, :event_key, :label_as_final, :display_date, keyword_init: true) do
       def get_binding
         data = self
@@ -26,21 +27,16 @@ module Manifest
       :ticket_name, :price, :surcharge, :per_ticket_let, 
       :quantity, :sum_subtotal, :sum_surcharge, 
       :sum_let_tax, :let_tax_rate_observed, 
+      :sum_sales_tax , :sum_venue_fee,
       # calculated fields
       :sum_bar_card, :subtotal_minus_bar_card,
 
       keyword_init: true) do
-        def total_face_value
-          self.sum_subtotal
-        end
         
         def total_let
           self.sum_let_tax
         end
 
-        def total_bar_card
-          "TOTAL_BAR_CARD_USED_HERE"
-        end
       end
   end
 end
