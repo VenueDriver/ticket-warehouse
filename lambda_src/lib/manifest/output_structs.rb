13e10dba3df2972a4c7c39553210d804f35f97e8
rcsteins <rcsteins@gmail.com>
2023-12-08 14:50:36 -0500

    Update output_structs.rb
    add calculated field sum_bard_card

--- lambda_src/lib/manifest/output_structs.rb
+++ lambda_src/lib/manifest/output_structs.rb
@@ -25,7 +25,11 @@ module Manifest
       :venue, :event_id, :event, :event_date, 
       :ticket_name, :price, :surcharge, :per_ticket_let, 
       :quantity, :sum_subtotal, :sum_surcharge, 
-      :sum_let_tax, :let_tax_rate_observed, keyword_init: true) do
+      :sum_let_tax, :let_tax_rate_observed, 
+      # calculated fields
+      :sum_bar_card,
+
+      keyword_init: true) do
         def total_face_value
           self.sum_subtotal
         end
