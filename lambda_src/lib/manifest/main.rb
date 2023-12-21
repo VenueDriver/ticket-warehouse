35a68d57e685c3ed20dadf6a59e31422e0cc1c40
rcsteins <rcsteins@gmail.com>
2023-12-21 15:38:47 -0500

    WIP Manifest Handler

--- lambda_src/lib/manifest/main.rb
+++ lambda_src/lib/manifest/main.rb
@@ -5,16 +5,32 @@ module Manifest
   class Main 
     class << self
 
-      def preliminary_report(event_id)
-        email_report = EmailReport.make_preliminary(event_id)
-        #email_report.send_ses_raw_email!($ses_client,to_addresses:EmailReport::TO_ALL)
+      def perform_report(event_id, report_variant_in, ses_client)
+        #placeholder
+
+        # if report_variant_in == 'preliminary'
+        #   Manifest::Main.preliminary_report(event_id,$ses_client)
+        # elsif report_variant_in == 'final'
+        #   Manifest::Main.final_report(event_id, $ses_client)
+        # else
+        #   raise "unknown report_variant: #{report_variant_in}"
+        # end
+
+        preliminary_report(event_id, ses_client)
       end
 
-      def final_report(event_id)
+      def preliminary_report(event_id, ses_client)
+        email_report = EmailReport.make_preliminary(event_id)
+        
+        email_report.send_ses_raw_email!(ses_client, to_addresses: EmailReport::MARTECH_TO)
+      end
+
+      def final_report(event_id, ses_client)
         final_report = EmailReport.make_final(event_id)
-        accounting_report = EmailReport.make_accounting(event_id)
-        
-        
+        accounting_report = EmailReport.make_accounting(event_id, to_addresses: EmailReport::MARTECH_TO)
+          
+        final_report.send_ses_raw_email!(ses_client)
+        accounting_report.send_ses_raw_email!(ses_client, to_addresses: EmailReport::MARTECH_TO )
       end
     end
   end
