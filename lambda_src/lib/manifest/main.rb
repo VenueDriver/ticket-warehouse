35126b1253ae87ed4000210cce217b688245c3dd
rcsteins <rcsteins@gmail.com>
2023-12-28 16:32:48 -0500

    Update main.rb

--- lambda_src/lib/manifest/main.rb
+++ lambda_src/lib/manifest/main.rb
@@ -5,20 +5,6 @@ module Manifest
   class Main 
     class << self
 
-      def perform_report(event_id, report_variant_in, ses_client)
-        #placeholder
-
-        # if report_variant_in == 'preliminary'
-        #   Manifest::Main.preliminary_report(event_id,$ses_client)
-        # elsif report_variant_in == 'final'
-        #   Manifest::Main.final_report(event_id, $ses_client)
-        # else
-        #   raise "unknown report_variant: #{report_variant_in}"
-        # end
-
-        preliminary_report(event_id, ses_client)
-      end
-
       def preliminary_report(event_id, ses_client)
         email_report = EmailReport.make_preliminary(event_id)
         
