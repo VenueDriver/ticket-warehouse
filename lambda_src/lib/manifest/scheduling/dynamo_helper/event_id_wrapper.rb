70a65c69b64856327f3b9dd90dcf82a7192a9651
rcsteins <rcsteins@gmail.com>
2023-12-29 17:09:54 -0500

    convert timestamps to strings

--- lambda_src/lib/manifest/scheduling/dynamo_helper/event_id_wrapper.rb
+++ lambda_src/lib/manifest/scheduling/dynamo_helper/event_id_wrapper.rb
@@ -19,26 +19,35 @@ module Manifest
       end
   
       def prelim_sent_update_expression( preliminary_sent_at: )
+        converted_preliminary_sent_at = convert_timestamp_to_string_for_dynamodb(preliminary_sent_at)
+        
         update_expression = 'SET report_status = :new_report_status, preliminary_sent_at = :preliminary_sent_at'
         expression_attribute_values = { 
           ':new_report_status' => PRELIM_SENT,
-          ':preliminary_sent_at' => preliminary_sent_at
+          ':preliminary_sent_at' => converted_preliminary_sent_at
         }
       
         create_update_params(update_expression, expression_attribute_values)
       end
   
       def final_sent_update_expression( final_sent_at: )
+        converted_final_sent_at = convert_timestamp_to_string_for_dynamodb(final_sent_at)
+        
         update_expression = 'SET report_status = :new_report_status, final_sent_at = :final_sent_at'
         expression_attribute_values = { 
           ':new_report_status' => FINAL_SENT,
-          ':final_sent_at' => final_sent_at
+          ':final_sent_at' => converted_final_sent_at
         }
       
         create_update_params(update_expression, expression_attribute_values)
       end
   
       private 
+
+      DDB_TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%LZ'
+      def convert_timestamp_to_string_for_dynamodb(timestamp)
+        timestamp.strftime(DDB_TIMESTAMP_FORMAT)
+      end
   
       def create_update_params( update_expression, expression_attribute_values, return_values: 'UPDATED_NEW')
         update_item_params = {
