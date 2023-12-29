806974c1d128b14bb3979e5b6f71b92b6a8d58db
rcsteins <rcsteins@gmail.com>
2023-12-29 16:44:18 -0500

    WIP

--- lambda_src/lib/manifest/scheduling/simulator.rb
+++ lambda_src/lib/manifest/scheduling/simulator.rb
@@ -6,66 +6,36 @@ module Manifest
         @dynamo_reader, @dynamo_writer = DynamoHelper.create_reader_and_writer(DEFAULT_DDB_TABLE_NAME)
       end
 
-      def demo_item_data
-        [{:event_key=>"fake_evvent_id_1", :report_status=>"initialized"},
-          {:event_key=>"fake_evvent_id_2", :report_status=>"initialized"}]
-      end
+      FAKE_EVENT_IDS = ['fake_evvent_id_1', 'fake_evvent_id_2', 'fake_evvent_id_3','fake_evvent_id_4']
 
-      def demo_put_requests
-        {"manifest_delivery_control-production"=>
-         [{:put_request=>{:item=>{:event_key=>"fake_evvent_id_1", :report_status=>"initialized"}}},
-          {:put_request=>{:item=>{:event_key=>"fake_evvent_id_2", :report_status=>"initialized"}}}]}
-      end
+      def try_read_fake_event_ids
+        fake_event_ids = FAKE_EVENT_IDS
 
-      def bad_put_requests
-        {{:table_name=>"manifest_delivery_control-production"}=>
-          [{:put_request=>{:item=>{:event_key=>"fake_evvent_id_3", :report_status=>"initialized"}}},
-          {:put_request=>{:item=>{:event_key=>"fake_evvent_id_4", :report_status=>"initialized"}}}]}
+        @dynamo_reader.fetch_control_rows(fake_event_ids)
       end
 
       def scratch
-        event_id = "654c5d04-b4c8-47f8-839d-573992144192"
-        @dynamo_writer.init_pending_reports([event_id])
-
-        @dynamo_writer.mark_preliminary_sent(event_id)
+        cleanup_control_rows
       end
 
-      def scratch_3
-        item_data = [{:event_key=>"fake_evvent_id_1", :report_status=>"initialized"},
-        {:event_key=>"fake_evvent_id_2", :report_status=>"initialized"}]
+      def cleanup_control_rows
+        rows = ['fake_evvent_id_1', 'fake_evvent_id_2']
 
-        put_requests = item_data.map do |single_item_data|
-          {put_request: { item:single_item_data}   }  
-        end
-        table_name = 'manifest_delivery_control-production'
-          
-        request_items = {
-          table_name => put_requests
-        }
-
-        dynamodb = Aws::DynamoDB::Client.new
-
-        dynamodb.batch_write_item(request_items: request_items)
-      end
-
-      def scratch_2
-        fake_event_ids = ['fake_evvent_id_1', 'fake_evvent_id_2']
-
-        report_status = Scheduling::CONTROL_INITIALIZED
-        table_name = Scheduling::DEFAULT_DDB_TABLE_NAME
-
-        event_ids = fake_event_ids
-
-        item_data = event_ids.map do |event_id|
-          {
-            event_key: event_id,
-            report_status: report_status
-          }
+        rows.each do |row|
+          @dynamo_writer.delete_control_row(row)
         end
 
-        item_data
+        recheck = @dynamo_reader.fetch_control_rows(rows)
+
+        puts "recheck: #{recheck}"
+
       end
 
+      def try_partition_event_ids_by_existence
+        fake_event_ids = FAKE_EVENT_IDS
+
+        @dynamo_reader.partition_event_by_exists_and_not_exists(fake_event_ids)
+      end
     end
   end
 end
@@ -89,15 +59,9 @@ end
 # @event_title="LEMA - Lowkey in the Library on Wednesdays",
 # @venue="Marquee Nightclub">]
 
-# def dyanmo_insert_jewel_jan_01_prelim
-#   event_id = "654c5d04-b4c8-47f8-839d-573992144192"
-#   @dynamo_writer.init_pending_reports([event_id])
+  # def scratch
+  #   event_id = "654c5d04-b4c8-47f8-839d-573992144192"
+  #   @dynamo_writer.init_pending_reports([event_id])
 
-#   #@dynamo_writer.mark_preliminary_sent(event_id)
-# end
-
-# def simulate_read_jewel_jan_01_control_row
-#   event_id = "654c5d04-b4c8-47f8-839d-573992144192"
-
-#   @dynamo_reader.fetch_control_rows([event_id])
-# end
+  #   @dynamo_writer.mark_preliminary_sent(event_id)
+  # end
