4d481522ddf159d3ac454583c60025fa8eae0104
rcsteins <rcsteins@gmail.com>
2023-12-29 17:09:59 -0500

    Update simulator.rb

--- lambda_src/lib/manifest/scheduling/simulator.rb
+++ lambda_src/lib/manifest/scheduling/simulator.rb
@@ -6,20 +6,36 @@ module Manifest
         @dynamo_reader, @dynamo_writer = DynamoHelper.create_reader_and_writer(DEFAULT_DDB_TABLE_NAME)
       end
 
-      FAKE_EVENT_IDS = ['fake_evvent_id_1', 'fake_evvent_id_2', 'fake_evvent_id_3','fake_evvent_id_4']
+      FAKE_EVENT_IDS_1 = ['fake_evvent_id_1', 'fake_evvent_id_2']
+      FAKE_EVENT_IDS_2 = ['fake_evvent_id_1', 'fake_evvent_id_2', 'fake_evvent_id_3','fake_evvent_id_4']
 
-      def try_read_fake_event_ids
-        fake_event_ids = FAKE_EVENT_IDS
-
-        @dynamo_reader.fetch_control_rows(fake_event_ids)
-      end
 
       def scratch
         cleanup_control_rows
+        mock_prelim_sent
       end
 
+      def mock_prelim_sent
+        event_ids = FAKE_EVENT_IDS_1
+
+        r = @dynamo_writer.init_pending_reports(event_ids)
+
+        recheck = @dynamo_reader.fetch_control_rows(event_ids)
+        puts "recheck1: #{recheck}"
+
+        event_ids.each do |event_id|
+          @dynamo_writer.mark_preliminary_sent(event_id)
+        end
+
+        recheck = @dynamo_reader.fetch_control_rows(event_ids)
+
+        puts "recheck2: #{recheck}"
+      end
+
+      ##########################################
+
       def cleanup_control_rows
-        rows = ['fake_evvent_id_1', 'fake_evvent_id_2']
+        rows = FAKE_EVENT_IDS_1
 
         rows.each do |row|
           @dynamo_writer.delete_control_row(row)
@@ -27,14 +43,19 @@ module Manifest
 
         recheck = @dynamo_reader.fetch_control_rows(rows)
 
-        puts "recheck: #{recheck}"
-
+       # puts "recheck: #{recheck}"
       end
 
       def try_partition_event_ids_by_existence
-        fake_event_ids = FAKE_EVENT_IDS
+        fake_event_ids = FAKE_EVENT_IDS_2
 
         @dynamo_reader.partition_event_by_exists_and_not_exists(fake_event_ids)
+      end
+
+      def try_read_fake_event_ids
+        fake_event_ids = FAKE_EVENT_IDS_1
+
+        @dynamo_reader.fetch_control_rows(fake_event_ids)
       end
     end
   end
