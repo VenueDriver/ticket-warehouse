90a0c23f91ed57a8502ef4ea54039cbc96633dc7
rcsteins <rcsteins@gmail.com>
2023-12-30 19:01:18 -0500

    include local start time in the rows for reference

--- lambda_src/lib/manifest/scheduling/candidate_event_row.rb
+++ lambda_src/lib/manifest/scheduling/candidate_event_row.rb
@@ -3,12 +3,13 @@ module Manifest
 
     class CandidateEventRow 
       attr_reader :venue, :event_id, :event_title
-      def initialize(venue:,event_id:,event_title:,event_date:,event_start_utc_timestamp:)
+      def initialize(venue:,event_id:,event_title:,event_date:,event_start_utc_timestamp:,local_event_start_timestamp:)
         @venue = venue
         @event_id = event_id
         @event_date = event_date
         @event_start_utc_timestamp = event_start_utc_timestamp
         @event_title = event_title
+        @local_event_start_timestamp = local_event_start_timestamp
       end
 
       def event_date_parsed
@@ -27,11 +28,13 @@ module Manifest
         event_date = hash_from_athena['event_date']
         event_title = hash_from_athena['event_title']
         event_start_utc_timestamp = hash_from_athena['event_start_utc_timestamp']
+        local_event_start_timestamp = hash_from_athena['local_event_start_timestamp']
         self.new(venue: venue, 
           event_id: event_id, 
           event_date: event_date, 
           event_title: event_title,
-          event_start_utc_timestamp: event_start_utc_timestamp)
+          event_start_utc_timestamp: event_start_utc_timestamp,
+          local_event_start_timestamp: local_event_start_timestamp)
       end
 
       def self.transform_event_rows(rows)
