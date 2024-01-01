00ebd46f02cb9930f75331441f66c6673bc6a54e
Ryan Alyn Porter <rap@endymion.com>
2024-01-01 21:14:00 +0000

    Parameters from URL parameters.

--- lambda_src/manifest-report-handler.rb
+++ lambda_src/manifest-report-handler.rb
@@ -1,17 +1,22 @@
-require_relative 'lib/manifest/manifest.rb'
-
 require 'json'
+require 'uri'
+require 'cgi'
+
+require_relative 'lib/manifest/manifest.rb'
 
 $ses_client = Aws::SES::Client.new(region:'us-east-1')
 
 def lambda_handler(event:, context:)
   puts "Manifest event: #{JSON.pretty_generate(event)}"
 
-  # report_variant param
-  # event_id param
-  event_id = event['event_id']
-  #report_variant_in = event['report_variant']
-  report_variant_in = 'preliminary'
+  referer_url = event['headers']['referer']
+  uri = URI.parse(referer_url)
+  params = CGI.parse(uri.query)
+
+  # If the parameters are not present in the event, extract them from the URL parameters
+  event_id = event['event_id'] || params['event_id'].first
+  report_variant_in = event['report_variant'] || params['report_variant'].first || 'preliminary'  
+
   Manifest::Main.perform_report(event_id, report_variant_in, $ses_client)
 
   { statusCode: 200, body: JSON.generate('ok') }
