ad1d233a992a255e84df766cc84b6bde6843c9dc
rcsteins <rcsteins@gmail.com>
2024-01-02 16:36:32 -0500

    WIP

--- lambda_src/lib/manifest/scheduling/dynamo_helper.rb
+++ lambda_src/lib/manifest/scheduling/dynamo_helper.rb
@@ -6,35 +6,10 @@ require_relative 'dynamo_helper/initial_row.rb'
 require_relative 'dynamo_helper/control_row.rb'
 require 'byebug'
 
+require_relative 'dynamo_helper/iniital_row.rb'
+
 module Manifest
   class Scheduling
-
-    class InitialRow
-  
-      class << self
-        def batch_CONTROL_INITIALIZED(event_ids, table_name:)
-          put_requests = event_ids.map do |event_id|
-            create_put_request(event_id)
-          end
-  
-          request_items = {
-            table_name => put_requests
-          } 
-        end
-
-        def create_put_request(event_id)
-          item_data = {
-            event_key: event_id,
-            report_status: CONTROL_INITIALIZED
-          }
-          
-          {
-            put_request: { item:item_data}   
-          }  
-        end
-  
-      end
-    end
   
     class DynamoWriter < DynamoHelperBase      
       def initialize_control_rows(event_ids)
@@ -80,20 +55,18 @@ module Manifest
       REMOVE_CONTROL_ROW_RETURN_VALUES = 'ALL_OLD'
 
       def delete_control_row(single_event_id)
-        begin
-          response = @dynamodb.delete_item({
-            table_name: self.table_name,
-            key: {
-              event_key: single_event_id
-            },
-            return_values: REMOVE_CONTROL_ROW_RETURN_VALUES
-          })
-          # [NONE, ALL_OLD, UPDATED_OLD, ALL_NEW, UPDATED_NEW]
-          puts "Delete successful. Deleted event_key #{single_event_id}, attributes: #{response.attributes}"
-          response
-        rescue Aws::DynamoDB::Errors::ServiceError => e
-          puts "Error deleting item: #{e.message}"
-        end
+        response = @dynamodb.delete_item({
+          table_name: self.table_name,
+          key: {
+            event_key: single_event_id
+          },
+          return_values: REMOVE_CONTROL_ROW_RETURN_VALUES
+        })
+        # [NONE, ALL_OLD, UPDATED_OLD, ALL_NEW, UPDATED_NEW]
+        puts "Delete successful. Deleted event_key #{single_event_id}, attributes: #{response.attributes}"
+        response
+      rescue Aws::DynamoDB::Errors::ServiceError => e
+        puts "Error deleting item: #{e.message}"
       end
   
       private 
--- lambda_src/lib/manifest/scheduling/dynamo_helper/initial_row.rb
+++ lambda_src/lib/manifest/scheduling/dynamo_helper/initial_row.rb
@@ -2,6 +2,31 @@ require_relative 'constants.rb'
 
 module Manifest
   class Scheduling
+    class InitialRow
   
+      class << self
+        def batch_CONTROL_INITIALIZED(event_ids, table_name:)
+          put_requests = event_ids.map do |event_id|
+            create_put_request(event_id)
+          end
+  
+          request_items = {
+            table_name => put_requests
+          } 
+        end
+
+        def create_put_request(event_id)
+          item_data = {
+            event_key: event_id,
+            report_status: CONTROL_INITIALIZED
+          }
+          
+          {
+            put_request: { item:item_data}   
+          }  
+        end
+  
+      end
+    end
   end
 end
