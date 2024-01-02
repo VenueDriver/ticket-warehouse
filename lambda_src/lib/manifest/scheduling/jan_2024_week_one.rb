08ebe16733221b4876c456625115c3693b189432
rcsteins <rcsteins@gmail.com>
2024-01-02 16:12:47 -0500

    Update jan_2024_week_one.rb
    set_jewel_jan_01_marked_final

--- lambda_src/lib/manifest/scheduling/jan_2024_week_one.rb
+++ lambda_src/lib/manifest/scheduling/jan_2024_week_one.rb
@@ -1,5 +1,36 @@
+require 'byebug'
+
 module Manifest
   class Scheduling
-    
+    class Jan2024WeekOne
+      EVENT_IDS = {
+        jewel_jan_01: '654c5d04-b4c8-47f8-839d-573992144192'
+      }
+
+      def initialize(env_in = 'production')
+        @candidate_event_reader = Manifest::Scheduling::CandidateEventReader.new(env_in)
+        @delivery_bookkeeper = Manifest::Scheduling::DeliveryBookkeeper.new(DEFAULT_DDB_TABLE_NAME)
+        @dynamo_reader = @delivery_bookkeeper.dynamo_reader
+      end
+
+      def set_jewel_jan_01_marked_final
+        event_id = nil ; # pending
+
+        event_id = EVENT_IDS[:jewel_jan_01]
+
+        r1 = @delivery_bookkeeper.process_preliminary_succeeded([event_id])
+        #byebug
+        #r1 = @dynamo_reader.fetch_control_row(event_id)
+
+        pp r1 
+
+        #byebug
+        r2 = @delivery_bookkeeper.process_final_succeeded([event_id])
+
+        pp r2
+
+      end
+    end
+
   end
 end
