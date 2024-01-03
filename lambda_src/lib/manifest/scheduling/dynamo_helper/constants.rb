1a3dbd7586ca7bd9adf71e8219fb37af16803283
rcsteins <rcsteins@gmail.com>
2024-01-03 10:55:05 -0500

    constant for indicating a null control row

--- lambda_src/lib/manifest/scheduling/dynamo_helper/constants.rb
+++ lambda_src/lib/manifest/scheduling/dynamo_helper/constants.rb
@@ -7,6 +7,7 @@ module Manifest
     PRELIM_SENT = 'prelim_sent'
     REPORT_CANCELED = 'report_canceled'
     FINAL_SENT = 'final_sent'
+    CONTROL_ROW_DOES_NOT_EXIST = 'control_row_does_not_exist'
   
     VALID_REPORT_STATUSES = [CONTROL_INITIALIZED, PRELIM_SENT, REPORT_CANCELED, FINAL_SENT]
   
