5efe9dc95b0b604a6c53cfe2f13d038973107d0f
rcsteins <rcsteins@gmail.com>
2024-01-03 18:37:03 -0500

    Update manager.rb
    add preview_schedule_for_pacific_time

--- lambda_src/lib/manifest/scheduling/manager.rb
+++ lambda_src/lib/manifest/scheduling/manager.rb
@@ -1,6 +1,10 @@
-#require 'ses'
-# need aws ses gem
 require 'aws-sdk-ses'
+require_relative 'report_selector.rb'
+require_relative 'report_performer.rb'
+require_relative 'delivery_bookkeeper.rb'
+require_relative 'preview_schedule.rb'
+
+require 'tzinfo'
 
 module Manifest
   class Scheduling
@@ -19,6 +23,20 @@ module Manifest
         categories = @report_selector.select_events(reference_time)
 
         categories
+      end
+
+      def preview_schedule_for_pacific_time(pacific_time_in)
+        pacific_time_zone = TZInfo::Timezone.get('America/Los_Angeles')
+        reference_time = pacific_time_zone.local_to_utc(pacific_time_in)
+
+        calculate_schedule_preview_using(reference_time)
+      end
+
+      def calculate_schedule_preview_using(reference_time = DateTime.now )
+        categorized_join_rows = self.calculate_report_selection_using(reference_time)
+
+        preview = PreviewSchedule.new(categorized_join_rows)
+        preview.summary_struct
       end
 
       def process_main_report_schedule_using(reference_time = DateTime.now)
