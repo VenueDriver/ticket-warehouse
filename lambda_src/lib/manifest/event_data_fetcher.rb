224851f1fe0b90daf5e5db9ec253ab272c0dc33f
Derek Norrbom <dnorrbom@hakkasan.com>
2024-02-14 14:54:23 -0800

    add rescue to mark preliminary sent
    add rescue to mark preliminary sent

--- lambda_src/lib/manifest/event_data_fetcher.rb
+++ lambda_src/lib/manifest/event_data_fetcher.rb
@@ -1,5 +1,6 @@
 require_relative '../../athena-manager.rb'
 require_relative 'dev_query.rb'
+require_relative 'scheduling/dynamo_writer.rb' # Import the DynamoWriter class
 
 module Manifest
   class EventDataFetcher
@@ -8,13 +9,23 @@ module Manifest
       @athena_manager = AthenaManager.new(env_in)
       @athena_manager.use_array_of_hashes_formatter!
       @query_builder = DevQuery.new
+      @dynamo_writer = DynamoWriter.new # Initialize an instance of DynamoWriter
     end
 
     def fetch_data_from_athena(event_id)
       query_string = @query_builder.on_event_id(event_id)
-      r = @athena_manager.start_query(query_string: query_string)
-      @last_query_result = r
-      r
+      begin
+        r = @athena_manager.start_query(query_string: query_string)
+        @last_query_result = r
+        r
+      rescue => e
+        # Log the error
+        puts "Error occurred: #{e.message}"
+        # Assuming there's a method or an instance variable to mark the event as preliminary sent
+        @dynamo_writer.mark_preliminary_sent(event_id)
+        # Return a default value or handle the error as needed
+        return []
+      end
     end
   end
 end
