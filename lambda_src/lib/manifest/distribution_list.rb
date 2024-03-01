838f7b42b957d49294b73d23522fd3085d94f584
Derek Norrbom <dnorrbom@hakkasan.com>
2024-03-01 13:38:15 -0800

    update key mapping for distro
    update key mapping for distro

--- lambda_src/lib/manifest/distribution_list.rb
+++ lambda_src/lib/manifest/distribution_list.rb
@@ -7,7 +7,7 @@ module Manifest
           "Liquid Pool Lounge"=>"VDNightlySalesLiquid@taogroup.com",
           "OMNIA"=>"VDNightlySalesOmnia@taogroup.com",
           "LAVO Italian Restaurant LV"=>"VDNightlySalesLavoLV@taogroup.com",
-          "Wet Republic"=>"VDNightlySalesWetRepublic@taogroup.com",
+          "Wet Republic Ultra Pool"=>"VDNightlySalesWetRepublic@taogroup.com",
           "Hakkasan Nightclub"=>"VDNightlySalesHakkasan@taogroup.com",
           "JEWEL Nightclub"=>"VDNightlySalesJewel@taogroup.com",
           "OMNIA San Diego"=>"VDNightlySalesOmnia@taogroup.com",
